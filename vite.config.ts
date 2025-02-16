12ad9d94d3bc4ec85e16b979fd458b78e03c15ad
michaeldavidvinc1 <tawie18102000@gmail.com>
2025-02-16 15:03:58 +0700

    Docker compose

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: '0.0.0.0',  // Biar bisa diakses dari luar container
+    port: 5173,
+    watch: {
+      usePolling: true,  // Biar Vite deteksi perubahan file dengan polling
+    },
+    strictPort: true,
+  }
 })
